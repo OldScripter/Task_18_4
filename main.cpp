95a43f12d05f80f779776bda4e941f45ac0130b8
OldScripter <Oldscripter@gmail.com>
2021-06-16 22:51:53 +0300

    EvenDigits function is added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void EvenDigits (long long number, int& ans)
+{
+    if (number == 0) return;
+
+    int digit = number % 10;
+    if (digit % 2 == 0) ans++;
+    EvenDigits(number / 10, ans);
+    return;
+}
+
+int main()
+{
+    int even_count = 0;
+    long long number = 9223372036854775806;
+
+    EvenDigits(number, even_count);
+
+    std::cout << "Number " << number << " contains: \n";
+    std::cout << even_count << " even digits\n";
+    
     return 0;
 }
